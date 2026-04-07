3a05eec9d2dd225ec16fda0ac1406554f02c7289
Gabriel Gausachs <g4a4b4r4i@gmail.com>
2026-04-07 16:58:35 +0200

    code prepared to run

--- Utils/feature_extraction.py
+++ Utils/feature_extraction.py
@@ -1,4 +1,6 @@
 import json
+import numpy as np
+import pandas as pd
 
 def get_passes(event_df, tracking_df):
     # Filter the event DataFrame to include only rows where:
@@ -22,12 +24,16 @@ def get_passes(event_df, tracking_df):
 
     return passes, tracking_passes
 
+
+
 def get_pass_frame(tracking_passes_df):
     tracking_passes_df["possession_start_frame"] = tracking_passes_df["possession_event"].apply(
         lambda x: x.get("start_frame") if isinstance(x, dict) else None)
     return tracking_passes_df
 
-def pressure_on_receiver(passes_df, tracking_passes_df, dict_games):
+
+
+def pressure_on_receiver(passes_df, tracking_passes_df, game):
     tracking_passes_df = get_pass_frame(tracking_passes_df)
     passes_df["pressure_on_receiver"] = np.nan  # Initialize the pressure column with NaN values
     for idx, row in tracking_passes_df.iterrows():
@@ -35,11 +41,6 @@ def pressure_on_receiver(passes_df, tracking_passes_df, dict_games):
         # Get the start frame of the possession event for this pass
         possession_start_frame = row.get("possession_start_frame")
         game_id = row.get("game_id")
-        if game_id not in dict_games:
-            print(f"Game ID {game_id} not found in dict_games. Skipping this pass.")
-            continue
-        game = dict_games[game_id]
-        game_tracking_frame = game.tracking_data[game.tracking_data["frame"] == possession_start_frame].iloc[0]
         
         # Get the receiver's name from the event data using the possession_event_id
         possession_event_id = row["possession_event_id"]
@@ -68,9 +69,135 @@ def pressure_on_receiver(passes_df, tracking_passes_df, dict_games):
             passes_df.loc[(passes_df['possessionEventId'] == possession_event_id) & (passes_df['gameId'] == game_id), 'pressure_on_receiver'] = pressure / 100  # Convert to percentage
     return passes_df
 
+
+
 def player_position_mapping(rosters_file, passes_df):
     with open(rosters_file, 'r') as f:
         rosters_data = json.load(f)
     nickname_to_position = {p['player']['nickname']: p['positionGroupType'] for p in rosters_data}
     passes_df['positionGroupType'] = passes_df['possessionEvents.targetPlayerName'].map(nickname_to_position)
     return passes_df
+
+
+
+def retain_lose_after_pass(passes_df, tracking_passes_df, tracking_df):
+    # Get succesfull passes
+    successfull_passes = passes_df[passes_df['is_success'] == True].copy()
+    passes_df["ball_lost_after"] = False
+
+    for idx, row in successfull_passes.iterrows():
+        # get the row in the tracking_passes_df with the same possession_event_id as the current row in successfull_passes
+        possession_event_id = row['possessionEventId']
+        game_id = row['gameId']
+        tracking_succ_pass = tracking_passes_df[(tracking_passes_df['possession_event_id'] == possession_event_id) & (tracking_passes_df['gameRefId'] == game_id)].iloc[0]
+        if not tracking_succ_pass.empty:
+            possession_start_frame = tracking_succ_pass.get("possession_start_frame")
+
+            # Get the next game_event_id
+            game_event_id = tracking_succ_pass["game_event_id"]
+            period = tracking_succ_pass["period"]
+            subset = tracking_df[
+                    (tracking_df["frameNum"] >= possession_start_frame) &
+                    (tracking_df["period"] == period) & (tracking_df["game_event_id"] != game_event_id) & tracking_df["game_event_id"].notnull()
+                ].copy()
+            subset["team_name"] = subset["game_event"].apply(
+                lambda x: x.get("team_name") if isinstance(x, dict) else None
+            )
+            subset["possession_event_type"] = subset["possession_event"].apply(
+                lambda x: x.get("possession_event_type") if isinstance(x, dict) else None
+            )
+            if not subset.empty:
+                # list of unique game_event_id in subset in order of appearance
+                unique_game_event_ids = subset["game_event_id"].unique()
+                # get the next and the second next game_event_id
+                next_game_event_id = unique_game_event_ids[0]
+
+                # get the rows of next_game_event_id
+                subset_next_game_event = subset[subset["game_event_id"] == next_game_event_id]
+                # if there is any row with possession_event_type equals to shot or cross, then we consider the pass as not lost
+                if subset_next_game_event["possession_event_type"].isin(["SH", "CR"]).any():
+                    continue
+                
+                second_next_game_event_id = unique_game_event_ids[1] if len(unique_game_event_ids) > 1 else None
+                team_name = subset[subset['game_event_id'] == next_game_event_id]['team_name'].iloc[0]
+                second_team_name = subset[subset['game_event_id'] == second_next_game_event_id]['team_name'].iloc[0] if second_next_game_event_id is not None else None
+                if second_team_name != team_name and second_team_name is not None:
+                    passes_df.loc[idx, "ball_lost_after"] = True
+    return passes_df
+
+
+
+def check_attacking_box(df, pitch_dims, home_team):
+    # Vectorized check
+    df['attacking_third'] = False
+    df['in_box'] = False
+    if home_team == 'home':
+        df['attacking_third'] = df['ball_x'] > pitch_dims[0]*2/3
+        df['in_box'] = df['ball_x'].between(36.0, 52.5) & df['ball_y'].between(-20.16, 20.16)
+    else:
+        df['attacking_third'] = df['ball_x'] < pitch_dims[0]/3
+        df['in_box'] = df['ball_x'].between(-52.5, -36.0) & df['ball_y'].between(-20.16, 20.16)
+    return df
+
+
+
+def what_leads(passes_df, tracking_passes_df, tracking_df, game):
+    successfull_retained_passes = passes_df[(passes_df['ball_lost_after'] == False) & (passes_df["is_success"] == True)].copy()
+    passes_df["attacking_third_entry"] = False
+    passes_df["box_entry"] = False
+    passes_df["shot_or_cross_after"] = False
+
+    for idx, row in successfull_retained_passes.iterrows():
+        # get the row in the tracking_passes_df with the same possession_event_id as the current row in successfull_retained_passes
+        possession_event_id = row['possessionEventId']
+        game_id = row['gameId']
+        tracking_succ_pass = tracking_passes_df[(tracking_passes_df['possession_event_id'] == possession_event_id) & (tracking_passes_df['gameRefId'] == game_id)].iloc[0]
+        if not tracking_succ_pass.empty:
+            possession_start_frame = tracking_succ_pass.get("possession_start_frame")
+
+            # Get the next game_event_id
+            game_event_id = tracking_succ_pass["game_event_id"]
+            period = tracking_succ_pass["period"]
+            game_clock = tracking_succ_pass["periodGameClockTime"]
+            team_name = tracking_succ_pass["game_event"].get("team_name") if isinstance(tracking_succ_pass["game_event"], dict) else None
+
+            # get position of the ball
+            ball_position = game.tracking_data[game.tracking_data["frame"] == int(possession_start_frame)].copy()
+            home_team = passes_df[passes_df['possessionEventId'] == possession_event_id]['gameEvents.homeTeam'].iloc[0]
+
+            # check if the ball is already in the attacking third
+            ball_position = check_attacking_box(ball_position, game.pitch_dimensions, home_team)
+            if ball_position['attacking_third'].iloc[0]:
+                attacking_third_start = True
+
+            subset = tracking_df[
+                    (tracking_df["frameNum"] >= possession_start_frame) & 
+                    (tracking_df["period"] == period) &
+                    (tracking_df["game_event_id"] != game_event_id) &
+                    tracking_df["game_event_id"].notnull() & (tracking_df["periodGameClockTime"] <= game_clock + 10)
+                ].copy()
+            subset["team_name"] = subset["game_event"].apply(
+                lambda x: x.get("team_name") if isinstance(x, dict) else None)
+            subset["possession_event_type"] = subset["possession_event"].apply(
+                lambda x: x.get("possession_event_type") if isinstance(x, dict) else None)
+            if not subset.empty:
+                # filter by team name
+                subset = subset[subset["team_name"] == team_name]
+                subset = subset.merge(
+                game.tracking_data[['frame', 'ball_x', 'ball_y']], 
+                    left_on='frameNum', right_on='frame', how='left')
+                subset = subset.dropna(subset=['ball_x', 'ball_y'])
+                subset = check_attacking_box(subset, game.pitch_dimensions, home_team)
+                if subset['attacking_third'].any() and not attacking_third_start:
+                    passes_df.loc[idx, "attacking_third_entry"] = True
+                if subset['in_box'].any():
+                    passes_df.loc[idx, "box_entry"] = True
+                
+                # if there is a shot or cross in the next 10 seconds
+                shot_or_cross = subset[(subset["possession_event_type"] == "SH") | (subset["possession_event_type"] == "CR")]
+                if not shot_or_cross.empty:
+                    passes_df.loc[idx, "shot_or_cross_after"] = True
+    
+    return passes_df
+
+
--- Utils/loading.py
+++ Utils/loading.py
@@ -9,27 +9,13 @@ def load_events_from_json(file_path):
         data = json.load(f)
     return pd.json_normalize(data)
 
-def load_files(base_path):
-    events_files = sorted([f for f in os.listdir(os.path.join(base_path, "eventdata")) if f.endswith('.json')])
-    tracking_parquet_files = sorted([f for f in os.listdir(os.path.join(base_path, "trackingdata_parquet")) if f.endswith('.parquet')])
-    
-    dict_events = {}
-    dict_tracking = {}
-    
-    for file_event, file_tracking in zip(events_files, tracking_parquet_files):
-        game_id = file_event.split(".")[0]
-        efile_path = os.path.join(base_path, "eventdata", file_event)
-        event_df = load_events_from_json(efile_path)
-        dict_events[game_id] = event_df
-        print("-" * 50)
-        print(f"Loaded {file_event}: {event_df.shape[0]} events")
-        
-        tfile = os.path.join(base_path, "trackingdata_parquet", file_tracking)
-        tracking_df = pd.read_parquet(tfile)
-        dict_tracking[game_id] = tracking_df
-        print(f"Loaded {file_tracking}: {tracking_df.shape[0]} rows")
-    
-    return dict_events, dict_tracking
+def load_files(event_path, tracking_path):
+    event_df = load_events_from_json(event_path )
+    print("-" * 50)
+    print(f"Loaded {event_path}: {event_df.shape[0]} events")
+    tracking_df = pd.read_parquet(tracking_path)
+    print(f"Loaded {tracking_path}: {tracking_df.shape[0]} rows")
+    return event_df, tracking_df
 
 def load_game_from_pff(base_path, game_id):
     dataset = pff.load_tracking(
